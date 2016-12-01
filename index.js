5c101f15aea2bfe872d471858f17fe114904b64a
Tom Clarkson <tom@orangeguava.com>
2016-12-01 14:04:38 +1100

    messages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import {Component, createElement} from "react";
 
 import { connect as rrc } from 'react-redux';
 
-export function connect(a, b, c) {
+export function connect(mapStateToProps, mapDispatchToProps, c) {
     if (global.__fbBatchedBridgeConfig) {
         // native host - render a web view and connect it with redux
         // just return settings
@@ -11,8 +11,8 @@ export function connect(a, b, c) {
         return function(component) {
             return {
                 url: "componenttest.js",
-                mapStateToProps: a,
-                mapDispatchToProps: b
+                mapStateToProps: mapStateToProps,
+                mapDispatchToProps: mapDispatchToProps
             };
         };
     } else if (global.isWrappedComponent) {
@@ -20,14 +20,39 @@ export function connect(a, b, c) {
         // inside the wrapped webview - return the unconnected component and wait for a message
         // implementation will be similar to react-redux - update props on message
         //alert(global.initialState);
+
+
+        let mappedFunctions = {};
+
+        if (mapDispatchToProps) {
+            mappedFunctions = mapDispatchToProps((action) => window.postMessage(JSON.stringify(action)));
+        }
+
         return function(InnerComponent) {
 
             class ConnectedComponent extends Component {
+                constructor() {
+                    super();
+                    this.receivedState = window.initialState;
+                    let component = this;
+                    document.addEventListener("message", function(e) {
+                        console.log("message received");
+                        console.log(e);
+                        component.receivedState.message = JSON.stringify(e.data);
+                        component.receivedState = Object.assign(component.receivedState, JSON.parse(e.data));
+                        component.setState({
+                            message3: JSON.stringify(e.data)
+                        });
+                    });
+
+                }
                 render() {
                     var initialProps = window.initialState;
 
                     return createElement(InnerComponent, {
-                        ...initialProps,
+                        ...this.receivedState,
+                        ...mappedFunctions,
+                        message3: this.props.message3,
                         ref: 'wrappedInstance'
                     });
                 }
@@ -37,6 +62,6 @@ export function connect(a, b, c) {
         };
     } else {
         // web component in normal use - call react redux
-        return rrc(a, b, c);
+        return rrc(mapStateToProps, mapDispatchToProps, c);
     }
 }
--- native.js
+++ native.js
@@ -5,12 +5,41 @@ import { WebView } from 'react-native';
 import { connect } from "react-redux";
 
 class MappedComponent extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.store = props.store || context.store;
+            // call store.getState so we can call mapStateToProps on it
+    }
+    processMessage(e) {
+        console.log("received message from webview");
+        var message = e.nativeEvent.data;
+        if (!message) return;
+        else if (message.indexOf("{") == 0) {
+            // action
+            this.store.dispatch(JSON.parse(message));
+        }
+        else {
+            console.log(message);
+        }
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+
+        var payload = {};
+        for (let i = 0; i < nextProps.propKeys.length; i++) {
+            let k = nextProps.propKeys[i];
+            payload[k] = nextProps[k];
+        }
+
+        console.log("Update of webview");
+        // todo: trim any unchanged values from the payload
+        this.webview.postMessage(JSON.stringify(payload));
+
+        return false;
+
+    }
     render() {
         //alert(this.props.component && this.props.component.url);
         //alert(this.props.testval);
-
-        var initialState = "{}";
-
 
         var payload = {};
         for (let i = 0; i < this.props.propKeys.length; i++) {
@@ -18,31 +47,34 @@ class MappedComponent extends Component {
             payload[k] = this.props[k];
         }
 
+        var initialState = JSON.stringify(payload);
 
-        if (!this.webview) {
-            // first load only
-            initialState = JSON.stringify(payload);
-        } else {
-            this.webview.postMessage(JSON.stringify(payload));
-        }
+        console.log("First load of webview");
+
+        var cssUrl = "http://localhost:8081/component.css";
+        var bundleUrl = "http://localhost:8081/componenttest.bundle?platform=ios&dev=true&minify=false";
 
         var source = {
-            html: "<body>Hello world5</body><script>window.process = {env: {}};window.isWrappedComponent = true; window.initialState=" + initialState + ";</script><script src='http://localhost:8081/componenttest.bundle?platform=ios&dev=true&minify=false'></script> "
+            html: "<html><head><link rel='stylesheet' href='" + cssUrl + "'></head><body>Hello world5</body><script>window.process = {env: {}};window.isWrappedComponent = true; window.initialState=" + initialState + ";</script><script src='" + bundleUrl + "'></script></html> "
         };
+
 
         return (
 
             <WebView ref = { webview => { this.webview = webview; } }
             source = { source }
+            onMessage = {(e) => this.processMessage(e)}
             style = {
-                { height: 250, width: 250 } }
+                { flex: 1, backgroundColor: "white" } }
             />
 
         );
     }
 }
 
-
+MappedComponent.contextTypes = {
+    store: React.PropTypes.any
+};
 
 class WebWrapper extends Component {
     constructor(props, context) {
@@ -60,7 +92,7 @@ class WebWrapper extends Component {
         this.ConnectedComponent = this.ConnectedComponent || connect(this.props.component.mapStateToProps, this.props.component.mapDispatchToProps)(MappedComponent);
         var ConnectedComponent = this.ConnectedComponent;
 
-        return ( <ConnectedComponent propKeys = { Object.keys(this.mappedProps) }
+        return (<ConnectedComponent propKeys = { Object.keys(this.mappedProps) }
             />
         );
     }
